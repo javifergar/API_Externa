9a9211f1f86974c285c843330ef3bdd6a646c4fc
Javifergar <javierfernandezgarcia123@gmail.com>
2025-09-17 18:58:25 +0200

    Home ,interfaces y services

--- src/app/interfaces/iuser.interface.ts
+++ src/app/interfaces/iuser.interface.ts
@@ -1,2 +1,17 @@
 export interface IUser {
+  _id: string;
+  id: string | number;
+  first_name: string;
+  last_name: string;
+  username: string;
+  email: string;
+  image: string;
+}
+
+export interface IResponse {
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+  results: IUser[];
 }
--- src/app/services/users-services.service.ts
+++ src/app/services/users-services.service.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@angular/core';
+// src/app/services/users-services.service.ts
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IResponse, IUser } from '../interfaces/iuser.interface';
+import { lastValueFrom } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class UsersServicesService {
-  
+  private http = inject(HttpClient);
+  private baseUrl = 'https://peticiones.online/api/users';
+
+  getAllUsers(page = 1, perPage = 10): Promise<IResponse> {
+    return lastValueFrom(
+      this.http.get<IResponse>(
+        `${this.baseUrl}?page=${page}&per_page=${perPage}`
+      )
+    );
+  }
+  getById(id: string | number): Promise<IUser> {
+    return lastValueFrom(this.http.get<IUser>(`${this.baseUrl}/${id}`));
+  }
+
+  createNewUser(user: IUser): Promise<IUser> {
+    return lastValueFrom(this.http.post<IUser>(this.baseUrl, user));
+  }
+  updateUser(id: string, user: IUser): Promise<IUser> {
+    return lastValueFrom(this.http.put<IUser>(`${this.baseUrl}/${id}`, user));
+  }
+  deleteUser(id: string): Promise<unknown> {
+    return lastValueFrom(this.http.delete(`${this.baseUrl}/${id}`));
+  }
 }
